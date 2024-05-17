e53046d7bd534cad09df116e558025666eccd025
parastoo saadat <136418045+parastooProgrammer@users.noreply.github.com>
2024-05-17 09:50:27 -0400

    Add files via upload
    first initial

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "come.selfstudy"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation ("come.selfstudy:listener:0.0.1-SNAPSHOT")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+//    mavenCentral()
+//    maven {
+//        url = uri("https://maven.pkg.github.com/parastooProgrammer/customSpringBootStarter")
+//        credentials {
+////            username = project.findProperty("gpr.user") as String? ?: System.getenv("gprUser")
+////            password = project.findProperty("gpr.key") as String? ?: System.getenv("gprKey")
+//            username = "parastooProgrammer"
+//            password = "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"
+//
+//        }
+//    }
---
+++ src/main/java/come/selfstudy/consumer/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package come.selfstudy.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"come.*"})
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}

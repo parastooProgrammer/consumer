b95cf435d3491b6ac048500f28ccf0f80f50d0fe
psaadat <parastoo.sa.1982@gmail.com>
2024-05-17 13:37:47 -0400

    changes

--- src/main/java/test/training/consumer/ConsumerApplication.java
+++ src/main/java/test/training/consumer/ConsumerApplication.java
@@ -3,10 +3,12 @@ package test.training.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan("come.*")
 public class ConsumerApplication {
 
     public static void main(String[] args) {
